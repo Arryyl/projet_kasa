d8e74bc1f8e305b34479ab5ae95c77c459578422
Arryyl <ettomaz7@gmail.com>
2023-10-11 01:48:18 +0200

    Test collapse

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,34 +1,31 @@
 // Import des styles et dépendances
 import "./Collapse.scss";
 import React, { useState } from "react";
-import { ChevronUp } from "../Icons/ChevronUp";
-import { ChevronDown } from "../Icons/ChevronDown";
+import ChevronUp  from "../../assets/arrow_down.svg";
 
 
 export default function Collapse({ items, customClass }) { // Definition du composant et ses "props"
-  const [isOpen, setIsOpen] = useState([false]); // Inisialisation du "state" par défaut
+  const [toggle, setToggle] = useState(false);
 
-  const toggleCollapse = (index) => { // Fonction douverture et de fermeture 
-    setIsOpen((prevOpenState) => {
-      const newOpenState = [...prevOpenState];
-      newOpenState[index] = !newOpenState[index];
-      return newOpenState;
-    });
-  };
+  const toggleState= () => {
+    setToggle(!toggle);
+    console.log("test")
+  }
 
   return ( // Rendu JSX en format accordéon (Mise en place des composants et utilisation de la fonction togglecollapse)
     <div className={`wrapper ${customClass}`}>
       <div className="accordion">
         {items.map((item, index) => (
           <div className="item" key={index}>
-            <div className="title" onClick={() => toggleCollapse(index)}>
+            <div className="title" onClick={toggleState}>
               <h2>{item.title}</h2>
-              <span className="chevron">
-                {isOpen[index] ? <ChevronUp /> : <ChevronDown />}
-              </span>
+              <img className={toggle ? "Chevron rotated": "Chevron"}
+                src={ChevronUp}
+                alt="Chevron"
+              />
             </div>
-            <div className={isOpen[index] ? "content show" : "content"}>
-              {item.paragraphe}
+            <div className={toggle ? "collapse_toggle animated": "collapse_toggle"}>
+              <p aria-hidden={toggle ? "true": "false"}>{item.paragraphe}</p>
             </div>
           </div>
         ))}
